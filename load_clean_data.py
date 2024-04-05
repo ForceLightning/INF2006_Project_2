bd04353713fd20d69902c0ed5b15b24f452a8963
jurgen-tan-git <56992789+jurgen-tan-git@users.noreply.github.com>
2024-04-05 20:44:26 +0800

    changed deduplication and convert columns to timestamps

--- load_clean_data.py
+++ load_clean_data.py
@@ -1,7 +1,7 @@
 import os
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import regexp_replace, split, when, count, desc, coalesce, lit, collect_list, col
+import pyspark.sql.functions as F
 from util import load_data
 
 DATA_DIR = "data"
@@ -42,23 +42,55 @@ def load_clean_data(
 
     df = load_data(data_dir=data_dir, spark_session=spark_session)
 
-    # Removing duplicates based on the tweet and id
-    df_deduplicated = df.dropDuplicates(["text", "_id"])
+    # Removing duplicates
+    df_deduplicated = df.dropDuplicates(
+        ["tweet_id", "_id", "text", "tweet_created", "name", "_created_at"]
+    )
+
+    print("Number of rows before deduplication: ", df.count())
+    print("Number of rows after deduplication: ", df_deduplicated.count())
+
+    # Converting to timestamps
+    df_deduplicated = df_deduplicated.withColumn(
+        "tweet_created", F.to_timestamp("tweet_created", "d/M/yyyy H:mm")
+    )
+
+    # Removal of the seconds from the timestamps
+    df_deduplicated = df_deduplicated.withColumn(
+        "_created_at",
+        F.when(
+            F.length(F.col("_created_at")) == 18, F.substring(F.col("_created_at"), 1, 15)
+        ).otherwise(F.col("_created_at")),
+    )
+
+    df_deduplicated = df_deduplicated.withColumn(
+        "_started_at",
+        F.when(
+            F.length(F.col("_started_at")) == 18, F.substring(F.col("_started_at"), 1, 15)
+        ).otherwise(F.col("_started_at")),
+    )
+
+    # Convert the processed timestamp string to a timestamp type
+    df_deduplicated = df_deduplicated.withColumn(
+        "_created_at", F.to_timestamp("_created_at", "M/d/yyyy H:mm")
+    )
+
+    df_deduplicated = df_deduplicated.withColumn(
+        "_started_at", F.to_timestamp("_started_at", "M/d/yyyy H:mm")
+    )
 
     # Remove \n and \r \t from the text column
     df_deduplicated = df_deduplicated.withColumn(
-        "text", regexp_replace("text", "\n|\r|\t", "")
+        "text", F.regexp_replace("text", "\n|\r|\t", "")
     )
 
     # Replace all double quotes with single quotes
-    df_deduplicated = df_deduplicated.withColumn(
-        "text", regexp_replace("text", '"', "'")
-    )
+    df_deduplicated = df_deduplicated.withColumn("text", F.regexp_replace("text", '"', "'"))
 
     # Filling missing values in the '_missed' column with False
     df_deduplicated = df_deduplicated.withColumn(
         "_missed",
-        when(df_deduplicated["_missed"].isNull(), False).otherwise(
+        F.when(df_deduplicated["_missed"].isNull(), False).otherwise(
             df_deduplicated["_missed"].cast("boolean")
         ),
     )
@@ -66,16 +98,15 @@ def load_clean_data(
     # Filling missing values in the 'retweet_count' column with 0
     df_deduplicated = df_deduplicated.withColumn(
         "retweet_count",
-        when(df_deduplicated["retweet_count"].isNull(), 0).otherwise(
+        F.when(df_deduplicated["retweet_count"].isNull(), 0).otherwise(
             df_deduplicated["retweet_count"]
         ),
     )
 
-    # Update 'airline_sentiment' column with values from 'airline_sentiment_gold'
-    # where 'airline_sentiment_gold' is not null
+    # Update 'airline_sentiment' column with values from 'airline_sentiment_gold' where 'airline_sentiment_gold' is not null
     df_deduplicated = df_deduplicated.withColumn(
         "airline_sentiment",
-        when(
+        F.when(
             df_deduplicated["airline_sentiment_gold"].isNotNull(),
             df_deduplicated["airline_sentiment_gold"],
         ).otherwise(df_deduplicated["airline_sentiment"]),
@@ -83,7 +114,7 @@ def load_clean_data(
 
     # Split the 'negativereason_gold' column by '&' and create new columns
     df_deduplicated = df_deduplicated.withColumn(
-        "negativereason_split", split("negativereason_gold", "\n")
+        "negativereason_split", F.split("negativereason_gold", "\n")
     )
 
     # Extract values from the split column into separate columns
@@ -94,9 +125,8 @@ def load_clean_data(
     # If negativereason1 is null, use the value in negativereason
     df_deduplicated = df_deduplicated.withColumn(
         "negativereason1",
-        when(
-            df_deduplicated["negativereason1"].isNull(),
-            df_deduplicated["negativereason"],
+        F.when(
+            df_deduplicated["negativereason1"].isNull(), df_deduplicated["negativereason"]
         ).otherwise(df_deduplicated["negativereason1"]),
     )
 
@@ -106,36 +136,54 @@ def load_clean_data(
     # Filling missing values in 'negativereason1' and 'negativereason2' with 'Unknown'
     df_deduplicated = df_deduplicated.withColumn(
         "negativereason1",
-        when(df_deduplicated["negativereason1"].isNull(), "Unknown").otherwise(
+        F.when(df_deduplicated["negativereason1"].isNull(), "Unknown").otherwise(
             df_deduplicated["negativereason1"]
         ),
     )
     df_deduplicated = df_deduplicated.withColumn(
         "negativereason2",
-        when(df_deduplicated["negativereason2"].isNull(), "Unknown").otherwise(
+        F.when(df_deduplicated["negativereason2"].isNull(), "Unknown").otherwise(
             df_deduplicated["negativereason2"]
         ),
     )
 
     # Group by country and user_timezone, count occurrences, and order by count in descending order
-    grouped_df = df_deduplicated.groupBy('_country', 'user_timezone').agg(count('*').alias('count')).orderBy('_country', desc('count'))
+    grouped_df = (
+        df_deduplicated.groupBy("_country", "user_timezone")
+        .agg(F.count("*").alias("count"))
+        .orderBy("_country", F.desc("count"))
+    )
 
     # Get the most common user_timezone for each country
-    most_common_user_timezone = grouped_df.groupBy('_country').agg(collect_list('user_timezone').getItem(0).alias('most_common_user_timezone'))
+    most_common_user_timezone = grouped_df.groupBy("_country").agg(
+        F.collect_list("user_timezone").getItem(0).alias("most_common_user_timezone")
+    )
 
     # Left join to fill missing values in 'user_timezone' with the most common value for the corresponding country
-    df_deduplicated = df_deduplicated.join(most_common_user_timezone, on='_country', how='left') \
-                                     .withColumn('user_timezone', coalesce(col('user_timezone'), col('most_common_user_timezone'), lit('Unknown')))
+    df_deduplicated = df_deduplicated.join(
+        most_common_user_timezone, on="_country", how="left"
+    ).withColumn(
+        "user_timezone",
+        F.coalesce(F.col("user_timezone"), F.col("most_common_user_timezone"), F.lit("Unknown")),
+    )
 
     # Drop the intermediate 'grouped_df' DataFrame
     grouped_df.unpersist()
 
+
     # Fill missing values in 'user_timezone' with 'Unknown'
     df_deduplicated = df_deduplicated.withColumn(
-        'user_timezone', when(col('user_timezone').isNull(), 'Unknown').otherwise(col('user_timezone'))
+        "user_timezone",
+        F.when(F.col("user_timezone").isNull(), "Unknown").otherwise(F.col("user_timezone")),
+    )
+
+    # Fill missing values in '_country' with 'Unknown'
+    df_deduplicated = df_deduplicated.withColumn(
+        "_country", F.when(F.col("_country").isNull(), "Unknown").otherwise(F.col("_country"))
     )
 
     # Drop inconsistent data columns and columns that have been split
+    # _tainted is dropped because all the values are False after deduplication
     df_deduplicated = df_deduplicated.drop(
         "airline_sentiment_gold",
         "negativereason_gold",
@@ -144,9 +192,10 @@ def load_clean_data(
         "_city",
         "tweet_location",
         "tweet_coord",
-        "_country",
-        "most_common_user_timezone"
+        "_tainted",
+        "most_common_user_timezone",
     )
+
 
     # Write the DataFrame to a CSV file
     df_deduplicated.repartition(1).write.csv(
