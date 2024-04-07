2b079c44a67951927fb048b8a52b86f0bc2ca923
SaltedEggYok <wong.yokhung@gmail.com>
2024-04-07 15:17:07 +0800

    cleaned up, commented, tried out a few more things with task 6

--- Task4.py
+++ Task4.py
@@ -1,4 +1,5 @@
 import findspark
+
 findspark.init()
 
 import pyspark
@@ -6,22 +7,27 @@ from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 from load_clean_data import load_clean_data
 
-# Task : To calculate the mean and median values of the trusting points for each channel.
-
-
+# Task 4 : To calculate the mean and median values of the trusting points for each channel.
 if __name__ == "__main__":
-    # Create a SparkSession
+    # create a SparkSession
     spark = SparkSession.builder.appName("Task4").getOrCreate()
 
-    # Load the data
+    # load the data
     df = load_clean_data("./data", spark)
-    
-    # Group by channel and calculate the mean and median of the trusting points
+
+    # group by channel and calculate the mean and median of the trusting points
     df_grouped = df.groupBy("_channel").agg(
         F.mean("_trust").alias("mean_trusting_points"),
-        F.median("_trust").alias("median_trusting_points")
+        F.median("_trust").alias("median_trusting_points"),
     )
-    
-    # Show the results
-    print(df_grouped.head(10))
-    
+
+    # DEBUG Show the results
+    # print(df_grouped.head(10))
+
+    # output results to a csv file
+    df_grouped.repartition(1).write.csv(
+        "task4_output_csv", header=True, mode="overwrite", quote='"'
+    )
+
+    # stop the spark session
+    spark.stop()
